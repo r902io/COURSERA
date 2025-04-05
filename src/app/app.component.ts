470c91afad43f947c4a87589939ccc2d84d63442
r902io <a20210905@pucp.edu.pe>
2025-04-05 18:07:59 -0500

    primer uso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent], //se agrega el componente
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true, //si no se usa el app.module.ts
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
